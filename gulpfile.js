2327f62e5d8c831f163e4d123db0ce249d88517e
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-08-08 00:55:30 -0300

    Added 'gulp-inject', 'wiredep-inject' and 'inject' tasks on 'gulpfile.js'.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,29 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
+var inject = require('gulp-inject');
+var wiredep = require('wiredep');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('inject',
+  ['gulp-inject',
+  'wiredep-inject']
+);
+
+gulp.task('gulp-inject', function () {
+  var target = gulp.src('./app/index.html');
+  // It's not necessary to read the files (will speed up things), we're only after their paths:
+  var sources = gulp.src(['./app/scripts/*.js', './app/styles/*.css'], {read: false});
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./app'));
+});
+
+gulp.task('wiredep-inject', function () {
+  wiredep({ src: 'app/index.html' });
 });
 
 // watch files for changes and reload
