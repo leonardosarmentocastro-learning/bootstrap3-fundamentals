18237b764ef8f172d29de0368d897b50b22402f4
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-08-07 22:26:05 -0300

    Added 'gulp.js' browserSync functionality.

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+// watch files for changes and reload
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
+
+  gulp.watch(['*.html', 'styles/**/*.css', 'scripts/**/*.js'], {cwd: 'app'}, reload);
+});
