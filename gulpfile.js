740dce6416bf89232fc363a42f490e25cd20e325
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-08-21 23:56:29 -0300

    'gulp-inject' fix.

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('gulp-inject', function () {
   // It's not necessary to read the files (will speed up things), we're only after their paths:
   var sources = gulp.src(['./app/scripts/*.js', './app/styles/*.css'], {read: false});
 
-  return target.pipe(inject(sources))
+  return target.pipe(inject(sources), { relative: true })
     .pipe(gulp.dest('./app'));
 });
 
